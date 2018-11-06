dd00a377932227cbf09ebd16d89af3826dc74244
Abdelrahman Mohamadeen <33037931+Abdo-A@users.noreply.github.com>
2018-11-07 01:34:55 +0200

    Update server.js

--- server.js
+++ server.js
@@ -20,6 +20,17 @@ mongoose
 //Use Routes
 app.use("/api/items", itemsRoutes);
 
+//Serve Static assets if in production
+if (process.env.NODE_END === "production") {
+  //Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
+//Listening
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
